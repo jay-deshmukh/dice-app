83f43b2790cfd25779e69972c0de535f443e48b6
Jay <jay>
2019-05-13 14:27:50 +0530

    Updates

---
+++ precache-manifest.bfff7f30fd15ff28cef41e102d9de568.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2798b7f6a81216dba7ba509091cc4571",
+    "url": "/dice-app/index.html"
+  },
+  {
+    "revision": "83bc1ae8a2984637bdff",
+    "url": "/dice-app/static/css/main.7a31dfc1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/dice-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "83bc1ae8a2984637bdff",
+    "url": "/dice-app/static/js/main.746e80d1.chunk.js"
+  },
+  {
+    "revision": "15f368191d94c6e2b1c5",
+    "url": "/dice-app/static/js/runtime~main.02aad080.js"
+  }
+]);
